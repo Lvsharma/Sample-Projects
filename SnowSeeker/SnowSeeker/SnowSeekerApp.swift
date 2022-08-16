f6629a4a523c7a376475d801c8507112722e9053
Luv Sharma <72157627+Lvsharma@users.noreply.github.com>
2022-08-16 21:57:02 +0530

    Add files via upload

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,14 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
